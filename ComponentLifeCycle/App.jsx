84f3c5369ce5d15e2a1e4240ec68e1f06eb79049
Tasfin <tasfin.hasan@fieldnation.com>
2017-07-25 21:00:33 +0600

    Added Component Lifecycle example

---
+++ ComponentLifeCycle/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Content from './Content.jsx'
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            "data" : 0
+        }
+
+        this.setNewNumber = this.setNewNumber.bind(this);
+
+        console.log('Component construct');
+    }
+
+    setNewNumber() {
+        this.setState({data: this.state.data + 1})
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick = {this.setNewNumber}>INCREMENT</button>
+                <Content myNumber = {this.state.data}></Content>
+            </div>
+        )
+    }
+}
+
+export default App;
